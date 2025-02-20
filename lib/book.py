00f63d88f2c888fbf48421286626586cb24e342b
PineApples11 <kinyanjuiivy1@gmail.com>
2025-02-20 15:22:02 +0300

    using decorators

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self.title=title
+        self.page_count=page_count
     
-        
+    @property
+    def page_count(self):
+        # print("Fetching page count....")
+        return self._page_count
+        # self._title
+
+    @page_count.setter
+    def page_count(self,page_count):
+        if type(page_count) == int:
+            # self._title=title
+            self._page_count=page_count
+            
+        else:
+            print("page_count must be an integer")
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+b = Book('The River and the Source',321) 
+b.page_count
+b.title
+    # page_count(page_count)
+# Book.page_count()
+# hubo=Book(title,page_count)
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
-
+# from book import Book
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def size(self):
+        print("Get shoe size ...")
+        return self._size
+
+    @size.setter
+    def size(self,size):
+        if type(size) == int:
+            self._size=size
+        else:
+            print("size must be an integer")
+
+    def repair(self):
+        print("shoe has been repaired")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        setattr(self,"condition","New")
+        return ("says that the shoe has been repaired.")
+
+s=Shoe("Jordan",23)
+setattr(s,"condition","New")
